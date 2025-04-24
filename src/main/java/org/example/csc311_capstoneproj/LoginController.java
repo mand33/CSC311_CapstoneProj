f68f1f24b70e4d7087586ea78b70db764fb9f89c
taolem13 <taolem13@gmail.com>
2025-04-24 19:34:26 -0400

    Login screen

---
+++ src/main/java/org/example/csc311_capstoneproj/LoginController.java
@@ -0,0 +1,11 @@
+package org.example.csc311_capstoneproj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button lgnButton;
+
+}
