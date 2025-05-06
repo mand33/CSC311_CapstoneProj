d427f9ce6030dad3867d79b4c094c8f3fd42f9c9
taolem13 <taolem13@gmail.com>
2025-05-05 23:56:21 -0400

    SceneManager

--- src/main/java/org/example/csc311_capstoneproj/App.java
+++ src/main/java/org/example/csc311_capstoneproj/App.java
@@ -5,18 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.csc311_capstoneproj.utils.SceneManager;
 
 import java.io.IOException;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("resources/fxml/login.fxml"));
-        Parent welcomeRoot = fxmlLoader.load();
-        Scene welcomeScene = new Scene(welcomeRoot);
-        stage.setTitle("Movie Diary");
-        stage.setScene(welcomeScene);
-        stage.show();
+    public void start(Stage primarystage) throws IOException {
+        SceneManager.setStage(primarystage);
+        SceneManager.switchTo("login.fxml");
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("resources/fxml/login.fxml"));
+//        Parent welcomeRoot = fxmlLoader.load();
+//        Scene welcomeScene = new Scene(welcomeRoot);
+        primarystage.setTitle("Movie Diary");
+//        primarystage.setScene(welcomeScene);
+//        primarystage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/csc311_capstoneproj/utils/SceneManager.java
@@ -0,0 +1,52 @@
+package org.example.csc311_capstoneproj.utils;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+public class SceneManager {
+    private static Stage primaryStage;
+
+    public static void setStage(Stage stage) {
+        primaryStage = stage;
+    }
+
+    /**
+     * Simple scene switch without passing data.
+     */
+    public static void switchTo(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(SceneManager.class.getResource("/resources/fxml/" + fxmlFile));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Scene switch that gives access to the controller to pass data.
+     * @param fxmlFile The FXML file to load.
+     * @param controllerConsumer A lambda that receives the controller.
+     * @param <T> The controller type.
+     */
+    public static <T> void switchToWithController(String fxmlFile, Consumer<T> controllerConsumer) {
+        try {
+            FXMLLoader loader = new FXMLLoader(SceneManager.class.getResource("/fxml/" + fxmlFile));
+            Parent root = loader.load();
+            T controller = loader.getController();
+            controllerConsumer.accept(controller); // Pass the controller to the caller
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
